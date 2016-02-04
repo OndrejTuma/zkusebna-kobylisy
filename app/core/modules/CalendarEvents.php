542edd98451764a314e0fb3676ae0f7af34bd6c1
Ondřej Tůma <ondr@centrum.cz>
2016-02-04 22:39:51 +0100

    Zprovozneni calendar eventu

--- app/core/modules/CalendarEvents.php
+++ app/core/modules/CalendarEvents.php
@@ -18,9 +18,11 @@ class CalendarEvents extends Zkusebna {
 			i.name as name,
 			image,
 			category,
-			r.name as reserved_by
+			c.name as reserved_by
 		FROM {$this->table_names["reservations"]} as r
-		LEFT JOIN {$this->table_names["items"]} as i ON r.item_id = i.id
+		LEFT JOIN {$this->table_names["r-i"]} as ri ON r.id = ri.reservation_id
+		LEFT JOIN {$this->table_names["items"]} as i ON ri.item_id = i.id
+		LEFT JOIN {$this->table_names["community"]} as c ON r.who = c.id
 		WHERE (r.date_from > '{$date_from}' OR r.date_to >= '{$date_from}') AND (r.date_from <= '{$date_to}' OR r.date_to < '{$date_to}')
 		ORDER BY category, parent_id, i.name";
 
